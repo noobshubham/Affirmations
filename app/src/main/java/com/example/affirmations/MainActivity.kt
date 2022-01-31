93ff81e85c15f8cbd2e39415046c18cce5907909
noobshubham <noobshubham65@gmail.com>
2022-01-31 11:40:14 +0530

    Minor Changes!

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.adapter.ItemAdapter
 import com.example.affirmations.data.Datasource
